1b2a812660f52afc04557b023f203b08f88b081b
Phuong H <phuonghqh@gmail.com>
2016-08-12 17:01:00 +0700

    Test CircleCI upload

--- setup.py
+++ setup.py
@@ -1,87 +1,93 @@
-
 import os
 import re
 
-# try:
-#     from semantic_release import setup_hook
-#     setup_hook(sys.argv)
-# except ImportError as e:
-#     print e
+import distutils.log
 
 try:
     import setuptools
 except ImportError:
     import distutils.core
+
     setup = distutils.core.setup
 else:
     setup = setuptools.setup
 
 
-def _get_version():
-    path = os.path.join(PATH_TO_FILE, 'ptestpypi', '__init__.py')
-    version_re = r".*__version__ = '(.*?)'"
-    fo = open(path)
-    try:
-        return re.compile(version_re, re.S).match(fo.read()).group(1)
-    finally:
-        fo.close()
+PACKAGE = next((str(s) for s in setuptools.find_packages('.', exclude=('tests', 'tests.*'))), None)
+PWD = os.path.abspath(os.path.dirname(__file__))
+VERSION = (
+    re
+        .compile(r".*__version__ = '(.*?)'", re.S)
+        .match(open(os.path.join(PWD, PACKAGE, '__init__.py')).read())
+        .group(1)
+)
+PYPI_USERNAME = os.environ['PYPI_USERNAME']
+PYPI_PASSWORD = os.environ['PYPI_PASSWORD']
+
+with open(os.path.join(PWD, 'README.md')) as f:
+    README = f.read()
+
+requires = [
+    'coreapi==1.20.0',
+    "-e git+https://github.com/finix-payments/wac@v0.26#egg=wac"
+]
+
+extras_require = {
+    'tests': [
+        'nose',
+        'coverage'
+    ]
+}
+
+scripts = [
+    # 'bin/citadel'
+]
 
 
-def _get_long_description():
-    path = os.path.join(PATH_TO_FILE, 'README.md')
-    fo = open(path)
-    try:
-        return fo.read()
-    finally:
-        fo.close()
+class UploadCommand(distutils.cmd.Command):
+    description = 'upload to PyPI'
+    user_options = []
 
+    def run(self):
+        from twine.commands import upload as twine_upload
+        self.announce('running upload %s to PyPI' % str(PACKAGE), level=distutils.log.INFO)
+        twine_upload.upload(
+            dists=['dist/*'],
+            repository='pypi',
+            sign=False,
+            identity=None,
+            username=PYPI_USERNAME,
+            password=PYPI_PASSWORD,
+            comment=None,
+            sign_with='gpg'
+        )
 
-def parse_requirements(file_name):
-    requirements = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'(\s*#)|(\s*$)', line):
-            continue
-        if re.match(r'\s*-e\s+', line):
-            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
-        elif re.match(r'\s*-f\s+', line):
-            pass
-        else:
-            requirements.append(line)
+    def initialize_options(self):
+        pass
 
-    return requirements
-
-
-def parse_dependency_links(file_name):
-    dependency_links = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'\s*-[ef]\s+', line):
-            dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
-
-    return dependency_links
-
-
-PATH_TO_FILE = os.path.dirname(__file__)
-VERSION = _get_version()
-LONG_DESCRIPTION = _get_long_description()
+    def finalize_options(self):
+        pass
 
 setup(
-    name='ptestpypi',
+    name=PACKAGE,
     version=VERSION,
-    url='https://ptestpypi.com/',
-    license='MIT License',
-    author='ptestpypi',
-    author_email='dev@ptestpypi.com',
-    description='Payments API',
-    long_description=LONG_DESCRIPTION,
-    packages=['ptestpypi'],
-    test_suite='nose.collector',
-    install_requires=parse_requirements('requirements.txt'),
-    tests_require=parse_requirements('test-requirements.txt'),
-    dependency_links=parse_dependency_links('requirements.txt'),
+    description='test app',
+    long_description=README,
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+        "Programming Language :: Python",
+    ],
+    author='Very Good',
+    author_email='dev@vgs.io',
+    license='MIT License',
+    packages=[PACKAGE],
+    include_package_data=True,
+    zip_safe=False,
+    scripts=scripts,
+    install_requires=requires,
+    extras_require=extras_require,
+    tests_require=extras_require['tests'],
+    test_suite='nose.collector',
+    cmdclass={
+        'upload': UploadCommand,
+    }
 )
